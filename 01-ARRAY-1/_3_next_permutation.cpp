f1a35ea9e260edaf539344e332f7fa8fccacea16
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-01 18:18:52 +0530

    Update _3_next_permutation.cpp

--- 01-ARRAY-1/_3_next_permutation.cpp
+++ 01-ARRAY-1/_3_next_permutation.cpp
@@ -4,42 +4,27 @@
 //link: https://leetcode.com/problems/next-permutation/
 
 
-//using Inbuilt Stl
-// class Solution {
-// public:
-//     void nextPermutation(vector<int>& nums) {
-//         next_permutation(nums.begin(),nums.end());
-//     }
-// };
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int bp=-1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                bp=i;
+                break;
+            }
+        }
+        if(bp!=-1){
+            for(int i=nums.size()-1;i>=0;i--){
+                if(nums[bp]<nums[i]){
+                    swap(nums[i],nums[bp]);
+                    break;
+                }
+            }
+        }
+        reverse(nums.begin()+bp+1,nums.end());
+    }
+};
 
-//using best approach O(n)
-// class Solution {
-// public:
-//     void nextPermutation(vector<int>& nums) {
-//         int n=nums.size();
-//         int b=-1;
-//         for(int i=n-2;i>=0;i--){
-//             if(nums[i]<nums[i+1]){
-//                 b=i;
-//                 break;
-//             }
-//         }
-//         if(b==-1){
-//             reverse(nums.begin(),nums.end());
-//             return;
-//         }
-      
-
-//         for(int j=n-1;j>b;j--){
-//             if(nums[j]>nums[b]){
-//                 int temp=nums[j];
-//                 nums[j]=nums[b];
-//                 nums[b]=temp;
-//                 break;
-//             }
-//         }
-
-//         reverse(nums.begin()+b+1,nums.end());
-//     }
-// };
-
+// tc:O(n)
+// sc:O(1)
