24c2d5ada1df9598a783697840037c06d91e4799
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-01 11:56:20 +0530

    Update _1_Set_matrix_zero.cpp

--- 01-ARRAY-1/_1_Set_matrix_zero.cpp
+++ 01-ARRAY-1/_1_Set_matrix_zero.cpp
@@ -1,7 +1,33 @@
 
 // Question1 :Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 // leetcode:73
-
+//tc:O(m*n)
+//sc:O(m+n)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<int>row(m,0);
+        vector<int>col(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]||col[j]){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+};
+//another optimal
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
