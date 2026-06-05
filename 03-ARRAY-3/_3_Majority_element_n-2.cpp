7acec0c24326052a3cce156153a3f75a037585e9
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-05 11:37:56 +0530

    Update _3_Majority_element_n-2.cpp

--- 03-ARRAY-3/_3_Majority_element_n-2.cpp
+++ 03-ARRAY-3/_3_Majority_element_n-2.cpp
@@ -4,26 +4,6 @@
 // // Leetcode:169  link: https://leetcode.com/problems/majority-element/description/
 
 
-#include<iostream>
-#include<vector>
-using namespace std;
-
-class Solution{
-    int majorityElement(vector<int>& nums) {
-        // //Brute force
-        // for(int i=0;i<nums.size();i++){
-        //     int c=1;
-        //     for(int j=i+1;j<nums.size();j++){
-        //         if(nums[j]==nums[i]){c++;}
-        //     }
-        //     if(c>nums.size()/2){
-        //         return nums[i];
-        //     }
-        // }
-        // return 0;
-
-
-
         //using map
         // map<int,int>m;
         for(int i=0;i<nums.size();i++){
@@ -38,26 +18,26 @@ class Solution{
 
 
         //Optimised Solution
-        int n = nums.size();
-        int cnt = 0;
-        int el;
+    class Solution {
+    public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0;
+        int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = nums[i];
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
             }
-            else if (el == nums[i]) cnt++;
-            else cnt--;
+
+            if (num == candidate)
+                count++;
+            else
+                count--;
         }
 
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el) cnt1++;
-        }
-
-        if (cnt1 > (n / 2)) return el;
-        return -1;
-
+        return candidate;
     }
 };
+
+// tc:0(n)
+// sc:0(1)    
