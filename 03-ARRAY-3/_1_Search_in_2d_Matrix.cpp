d5df6d46bbe9de83ab3c17169408bbd05a464530
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-05 11:10:24 +0530

    Update _1_Search_in_2d_Matrix.cpp

--- 03-ARRAY-3/_1_Search_in_2d_Matrix.cpp
+++ 03-ARRAY-3/_1_Search_in_2d_Matrix.cpp
@@ -7,36 +7,34 @@
 //Leetcode:74 link:https://leetcode.com/problems/search-a-2d-matrix/description/
 
 
-#include<iostream>
-#include<vector>
-using namespace std;
-
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
-        int r=matrix.size();
-        int c=matrix[0].size();
-
-        if(r==0 || c==0){
-            return false;
-        }
+        int m = matrix.size();
+        int n = matrix[0].size();
 
         int low = 0;
-        int high= (r*c) - 1;
-        
-        while(low <= high) {
-            int mid = (low + (high - low) / 2);
-            if(matrix[mid/c][mid % c] == target) {
+        int high = m * n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matrix[row][col] == target)
                 return true;
-            }
-            if(matrix[mid/c][mid % c] < target) {
+
+            if (matrix[row][col] < target)
                 low = mid + 1;
-            }
-            else {
+            else
                 high = mid - 1;
-            }
         }
-        return false;
+
+        return false;    
     }
 };
+
+// Complexity
+// Time Complexity: O(log(m*n))
+// Space Complexity: O(1)
