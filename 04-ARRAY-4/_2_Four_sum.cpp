d2c56ab17633856c8701c9b3be32f64e59226cfe
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-07 08:35:50 +0530

    Update _2_Four_sum.cpp

--- 04-ARRAY-4/_2_Four_sum.cpp
+++ 04-ARRAY-4/_2_Four_sum.cpp
@@ -37,37 +37,68 @@ public:
         // return ans;
 
 
-        //Optimised Solution using two pointers
-        set<vector<int>>answer;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(i>1 && nums[i]==nums[i-1]){continue;}
-            for(int j=i+1;j<n;j++){
-                if(j>i+1 && nums[j]==nums[j-1]){continue;}
-                
-                int k=j+1;
-                int l=n-1;
-                while(k<l){
-                    long long sum=nums[i]+nums[j];
-                    sum+=nums[k];
-                    sum+=nums[l];
-                    if(sum==target){
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
-                        answer.insert(temp);
-                        k++;
-                        l--;
+    //Optimised Solution using two pointers
+    class Solution {
+    public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> ans;
 
-                        while(k<l && nums[k]==nums[k-1]){k++;}
-                        while(k<l && nums[l]==nums[l+1]){l--;}
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long long sum =
+                        (long long)nums[i] +
+                        nums[j] +
+                        nums[left] +
+                        nums[right];
+
+                    if (sum == target) {
+                        ans.push_back(
+                            {nums[i], nums[j], nums[left], nums[right]}
+                        );
+
+                        left++;
+                        right--;
+
+                        while (left < right &&
+                               nums[left] == nums[left - 1])
+                            left++;
+
+                        while (left < right &&
+                               nums[right] == nums[right + 1])
+                            right--;
                     }
-                    else if(sum<target){k++;}
-                    else{l--;}
+                    else if (sum < target) {
+                        left++;
+                    }
+                    else {
+                        right--;
+                    }
                 }
             }
         }
-        vector<vector<int>>ans={answer.begin(),answer.end()};
-        return ans;
 
+        return ans;
     }
 };
+
+// tc:0(n^3)
+// sc:0(1)
+
+// Pattern
+
+// This is a direct extension of Two Sum → 3Sum → 4Sum:
+
+// Two Sum → Hash Map / Two Pointers → O(n)
+// 3Sum → Fix one + Two Pointers → O(n²)
+// 4Sum → Fix two + Two Pointers → O(n³)
