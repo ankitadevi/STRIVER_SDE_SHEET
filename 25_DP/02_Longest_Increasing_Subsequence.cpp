ddddd9e12a5041085df7813d5deb88016689aae9
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-09 11:09:02 +0530

    Update 02_Longest_Increasing_Subsequence.cpp

--- 25_DP/02_Longest_Increasing_Subsequence.cpp
+++ 25_DP/02_Longest_Increasing_Subsequence.cpp
@@ -1,89 +1,103 @@
-// Longest Increasing Subsequence | (DP-41)
-// In the coming articles, we will discuss problems related to ‘Longest Increasing Subsequence’. Before proceeding further, let us understand the “Longest Increasing Subsequence”, or rather what is a “subsequence”?
-// A subsequence of an array is a list of elements of the array where some elements are deleted ( or not deleted at all) and they should be in the same order in the subsequence as in the original array.
-// For example, for the array: [2,3,1] , the subsequences will be [{2},{3},{1},{2,3},{2,1},{3,1},{2,3,1}} but {3,2} is not a subsequence because its elements are not in the same order as the original array.
+// recurrsion
+int solve(int i, int j,
+          string& s1,
+          string& s2){
 
-//Leetcode: 300 link:https://leetcode.com/problems/longest-increasing-subsequence/description/
+    if(i == s1.size() || j == s2.size())
+        return 0;
 
-// Brute Force (Recursion)
+    if(s1[i] == s2[j])
+        return 1 + solve(i+1,j+1,s1,s2);
+
+    return max(
+        solve(i+1,j,s1,s2),
+        solve(i,j+1,s1,s2)
+    );
+}
+// memo
 class Solution {
 public:
-    int solve(int ind, int prev, vector<int>& nums) {
-        if(ind == nums.size()) return 0;
 
-        int notTake = solve(ind + 1, prev, nums);
+    int solve(int i, int j,
+              string& s1,
+              string& s2,
+              vector<vector<int>>& dp){
 
-        int take = 0;
-        if(prev == -1 || nums[ind] > nums[prev]) {
-            take = 1 + solve(ind + 1, ind, nums);
+        if(i == s1.size() || j == s2.size())
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        if(s1[i] == s2[j]){
+
+            return dp[i][j] =
+                1 + solve(i+1,j+1,s1,s2,dp);
         }
 
-        return max(take, notTake);
+        return dp[i][j] =
+            max(
+                solve(i+1,j,s1,s2,dp),
+                solve(i,j+1,s1,s2,dp)
+            );
     }
 
-    int lengthOfLIS(vector<int>& nums) {
-        return solve(0, -1, nums);
-    }
-};
-// Complexity
-// Time: O(2ⁿ)
-// Space: O(n) (recursion stack)
+    int lcs(string str1, string str2) {
 
-// DP Memoization (Top-Down)
-class Solution {
-public:
-    int solve(int ind, int prev, vector<int>& nums,
-              vector<vector<int>>& dp) {
+        int n = str1.size();
+        int m = str2.size();
 
-        if(ind == nums.size()) return 0;
+        vector<vector<int>> dp(
+            n,
+            vector<int>(m,-1)
+        );
 
-        if(dp[ind][prev + 1] != -1)
-            return dp[ind][prev + 1]; 
-
-        int notTake = solve(ind + 1, prev, nums, dp);
-
-        int take = 0;
-        if(prev == -1 || nums[ind] > nums[prev]) {
-            take = 1 + solve(ind + 1, ind, nums, dp);
-        }
-
-        return dp[ind][prev + 1] = max(take, notTake);
-    }
-
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1)); 
-
-        return solve(0, -1, nums, dp);
+        return solve(
+            0,0,
+            str1,str2,
+            dp
+        );
     }
 };
-// Complexity
-// Time: O(n²)
-// Space: O(n²) + O(n) recursion stack
-
-// DP Tabulation (Bottom-Up)
+// tab
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
 
-        vector<int> dp(n, 1);
+    int lcs(string str1, string str2) {
 
-        int ans = 1;
+        int n = str1.size();
+        int m = str2.size();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if(nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
+        vector<vector<int>> dp(
+            n + 1,
+            vector<int>(m + 1, 0)
+        );
+
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j <= m; j++){
+
+                if(str1[i-1] == str2[j-1]){
+
+                    dp[i][j] =
+                        1 + dp[i-1][j-1];
+                }
+                else{
+
+                    dp[i][j] =
+                        max(dp[i-1][j],
+                            dp[i][j-1]);
                 }
             }
-            ans = max(ans, dp[i]);
         }
 
-        return ans;
+        return dp[n][m];
     }
 };
-// Complexity
-// Time: O(n²)
-// Space: O(n)
+
+// | Approach        | Time       | Space         |
+// | --------------- | ---------- | ------------- |
+// | Recursion       | O(2^(n+m)) | O(n+m)        |
+// | Memoization     | O(n*m)     | O(n*m)+O(n+m) |
+// | Tabulation      | O(n*m)     | O(n*m)        |
+// | Space Optimized | O(n*m)     | O(m)          |
