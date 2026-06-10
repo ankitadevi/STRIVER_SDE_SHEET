1805dbb83dda501b86ceb44b1443a34ea20b25db
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-10 09:21:40 +0530

    Update 04_0-1-KnapSack.cpp

--- 25_DP/04_0-1-KnapSack.cpp
+++ 25_DP/04_0-1-KnapSack.cpp
@@ -1,91 +1,113 @@
+// recursion
 
-// A thief wants to rob a store. 
-//He is carrying a bag of capacity W. 
-//The store has ‘n’ items. 
-//Its weight is given by the ‘wt’ array and its value by the ‘val’ array. 
-//He can either include an item in its knapsack or exclude it but can’t partially have it as a fraction. 
-//We need to find the maximum value of items that the thief can steal.
+class Solution{
+public:
 
-//leetcode: no
-//gfg: https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+    int solve(int idx, int W,
+              vector<int>& wt,
+              vector<int>& val){
 
+        if(idx == wt.size()) return 0;
 
-#include<iostream>
-using namespace std;
+        int notTake = solve(idx + 1, W, wt, val);
 
-class solution{
-    public:
-    //Function to return max value that can be put in knapsack of capacity W.
-     
-     
-    // DP memoization
-    
-    int knap(int wt[],int val[],int n,int w,vector<vector<int>>&dp){
-        if(n==0){
-            if(wt[0]<=w){
-                return val[0];
-            }
-             return 0;
-        }
-        
-        if(dp[n][w]!=-1)return dp[n][w];
-        
-        int no = 0 + knap(wt,val,n-1,w,dp);
-        int yes = INT_MIN;
-        if(wt[n] <= w)yes= val[n] + knap(wt,val,n-1,w-wt[n],dp);
-        
-        return dp[n][w] = max(no,yes);
+        int take = 0;
+        if(wt[idx] <= W)
+            take = val[idx] +
+                   solve(idx + 1,
+                         W - wt[idx],
+                         wt, val);
+
+        return max(take, notTake);
     }
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-        vector<vector<int>>dp(n,vector<int>(W+1,-1));
-        return knap(wt, val, n-1, W, dp);
-    }
-    
-    
-//Tabulation (space optimised)
-//  int knapsack(vector<int>& wt, vector<int>& val, int n, int W){
-//       vector<vector<int>> dp(n,vector<int>(W+1,0));
-//       for(int i=wt[0]; i<=W; i++){
-//           dp[0][i] = val[0];
-//       }
-         
-//       for(int ind =1; ind<n; ind++){
-//           for(int cap=0; cap<=W; cap++){
-//               int notTaken = 0 + dp[ind-1][cap];
-//               int taken = INT_MIN;
-//               if(wt[ind] <= cap)
-//                   taken = val[ind] + dp[ind-1][cap - wt[ind]];
-//               dp[ind][cap] = max(notTaken, taken);
-//           }
-//       }
-//       return dp[n-1][W];
-     // }
 
-    
-    // using Recursion TLE   
-    // void knap(int s,int tv,int tw,int w,int wt[],int val[],int n,int &maxi){
-    //     if(s==n || tw>w){
-    //         if(tw<=w){
-    //             maxi=max(maxi,tv);
-    //         }
-    //         return;
-    //     }
-        
-    //     maxi=max(tv,maxi);
-        
-    //     tv+=val[s];
-    //     tw+=wt[s];
-    //     knap(s+1,tv,tw,w,wt,val,n,maxi);
-    //     tv-=val[s];
-    //     tw-=wt[s];
-    //     knap(s+1,tv,tw,w,wt,val,n,maxi);
-    // }
-    
-    // int knapSack(int W, int wt[], int val[], int n) 
-    // { 
-    //     int maxi=0;
-    //     knap(0,0,0,W,wt,val,n,maxi);
-    //     return maxi;
-    // }
+    int knapsack01(vector<int>& wt,
+                   vector<int>& val,
+                   int n, int W) {
+
+        return solve(0, W, wt, val);
+    }
 };
+
+// memo
+
+class Solution{
+public:
+
+    int solve(int idx, int W,
+              vector<int>& wt,
+              vector<int>& val,
+              vector<vector<int>>& dp){
+
+        if(idx == wt.size()) return 0;
+
+        if(dp[idx][W] != -1)
+            return dp[idx][W];
+
+        int notTake =
+            solve(idx + 1, W, wt, val, dp);
+
+        int take = 0;
+        if(wt[idx] <= W)
+            take = val[idx] +
+                   solve(idx + 1,
+                         W - wt[idx],
+                         wt, val, dp);
+
+        return dp[idx][W] =
+               max(take, notTake);
+    }
+
+    int knapsack01(vector<int>& wt,
+                   vector<int>& val,
+                   int n, int W) {
+
+        vector<vector<int>> dp(
+            n, vector<int>(W + 1, -1));
+
+        return solve(0, W, wt, val, dp);
+    }
+};
+
+// tab
+
+class Solution{
+public:
+
+    int knapsack01(vector<int>& wt,
+                   vector<int>& val,
+                   int n, int W) {
+
+        vector<vector<int>> dp(
+            n, vector<int>(W + 1, 0));
+
+        for(int w = wt[0]; w <= W; w++)
+            dp[0][w] = val[0];
+
+        for(int i = 1; i < n; i++){
+
+            for(int w = 0; w <= W; w++){
+
+                int notTake = dp[i-1][w];
+
+                int take = 0;
+
+                if(wt[i] <= w)
+                    take = val[i] +
+                           dp[i-1][w - wt[i]];
+
+                dp[i][w] =
+                    max(take, notTake);
+            }
+        }
+
+        return dp[n-1][W];
+    }
+};
+
+// | Approach        | Time Complexity | Space Complexity     |
+// | --------------- | --------------- | -------------------- |
+// | Recursion       | (O(2^n))        | (O(n))               |
+// | Memoization     | (O(n \times W)) | (O(n \times W)+O(n)) |
+// | Tabulation      | (O(n \times W)) | (O(n \times W))      |
+// | Space Optimized | (O(n \times W)) | (O(W))               |
