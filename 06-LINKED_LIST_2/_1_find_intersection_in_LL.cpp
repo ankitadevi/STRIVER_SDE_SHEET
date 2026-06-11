e4f29c84584ebf3f886171425f10315d0c2264f6
ANKITA DEVI <ad2004ankita@gmail.com>
2026-06-11 09:53:44 +0530

    Update _1_find_intersection_in_LL.cpp

--- 06-LINKED_LIST_2/_1_find_intersection_in_LL.cpp
+++ 06-LINKED_LIST_2/_1_find_intersection_in_LL.cpp
@@ -1,98 +1,47 @@
-// Find intersection of Two Linked Lists
-// Problem Statement: Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
-// If the two linked lists have no intersection at all, return null.
-
-//Leetcode:160  link:https://leetcode.com/problems/intersection-of-two-linked-lists/
-
-
-#include<iostream>
-using namespace std;
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
+// brute-force
 class Solution {
 public:
-
-    //Using Brute force Approach
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* l1=headA;
-        ListNode* l2=headB;
-        while(l1){
-            while(l2){
-                if(l1==l2){
-                    return l1;
+        
+        ListNode* currA = headA;
+
+        while (currA) {
+            
+            ListNode* currB = headB;
+
+            while (currB) {
+                if (currA == currB) {
+                    return currA;
                 }
-                l2=l2->next;
+                currB = currB->next;
             }
-            l2=headB;
-            l1=l1->next;
+
+            currA = currA->next;
         }
+
         return NULL;
-    }
-
-
-    //Using hashMap
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-        unordered_set<ListNode*>s;
-        ListNode* l1=headA;
-        ListNode* l2=headB;
-        while(l1){
-            s.insert(l1);
-            l1=l1->next;
-        }
-        while(l2){
-            if(s.find(l2)!=s.end()){
-                return l2;
-            }
-            l2=l2->next;
-        }
-        return NULL;
-    }
-
-
-    //using calculating the difference
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-        ListNode* l1=headA;
-        ListNode* l2=headB;
-        int d1=0,d2=0;
-        while(l1 || l2){
-            if(l1){
-                d1++;
-                l1=l1->next;
-            }
-            if(l2){
-                d2++;
-                l2=l2->next;
-            }
-        }
-        l1=headA;
-        l2=headB;
-        int diff=d1-d2;
-        if(diff < 0) 
-            while(diff++ != 0) l2 = l2->next; 
-        else while(diff-- != 0) l1 = l1->next;
-
-        while(l1 != NULL) {
-            if(l1 == l2) return l1;
-            l2 = l2->next;
-            l1 = l1->next;
-        }
-        return l1;
-    }
-
-
-    //optimised solution
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-        ListNode* l1=headA;
-        ListNode* l2=headB;
-
-        while(l1!=l2){
-            l1=l1==NULL?headB:l1->next;
-            l2=l2==NULL?headA:l2->next;
-        }
-        return l1;
     }
 };
+// Time Complexity: O(m × n)
+// For each node of list A, we traverse all nodes of list B.
+// Space Complexity: O(1)
+
+// optimal
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while (a != b) {
+            a = (a == NULL) ? headB : a->next;
+            b = (b == NULL) ? headA : b->next;
+        }
+
+        return a; // intersection node or NULL
+    }
+};
+// Complexity
+// Time: O(m + n)
+// Space: O(1)
